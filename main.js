585dee641317214b41a78a0b88a041d1287e916f
Aloïs Deniel <alois.deniel@gmail.com>
2015-09-30 23:54:47 +0200

    Added new cards, improved data declaration, tag filtering via query string.

--- main.js
+++ main.js
@@ -1,16 +1,54 @@
+(function($) {
+    $.QueryString = (function(a) {
+        if (a == "") return {};
+        var b = {};
+        for (var i = 0; i < a.length; ++i)
+        {
+            var p=a[i].split('=');
+            if (p.length != 2) continue;
+            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+        }
+        return b;
+    })(window.location.search.substr(1).split('&'))
+})(jQuery);
+
 // 1. Parsing snippets
+
+var tags = $.QueryString["tags"];
+
+if(tags)
+{
+  tags = tags.split(',');
+}
+else { tags = null; }
 
 var allSnippets = [];
 var allTags = [];
 
-var snippetsNodes = $("code.snippet").each(function(index ) {
+var snippetsNodes = $("script.snippet").each(function(index ) {
+
+var snippetTags = $(this).data("tags").toString().split(',');
+
+var isValid = tags == null;
+
+if(!isValid)
+{
+  for (var i = 0; i < snippetTags.length && !isValid; i++) {
+    isValid = $.inArray(snippetTags[i],tags) >= 0;
+  }
+}
+
+if(isValid)
+{
   allSnippets.push({
-      title: $(this).attr("cc-title"),
-      lang: $(this).attr("cc-lang"),
-      description: $(this).attr("cc-description"),
-      tags: $(this).attr("cc-tags").split(','),
+      title: $(this).data("title"),
+      lang: $(this).data("lang"),
+      description: $(this).data("description"),
+      tags: snippetTags,
       content: $(this).html()
   });
+}
+
 });
 
 // 2. Rendering view
@@ -32,9 +70,8 @@ function appendSnippet(content,lastSnippet){
 
     //div += "<div class='badge badge-" + index + "'></div>";
     //tagValues += "<span class=tag-" + index + ">"+  allTags[index - 1] + "</span>";
-    div += "<span class='tag tag-" + index + "''>"+  allTags[index - 1] + "</span>";
+    div += "<span class='tag tag-" + ((index - 1)  % 10) + "''>"+  allTags[index - 1] + "</span>";
   }
-
 
   div += "<span>"+lastSnippet.title+"</span></h2><p>"+lastSnippet.description+"</p><pre><code class='"+lastSnippet.lang+"'>"+lastSnippet.content+"</code></pre></div>";
   content.innerHTML += div;
