8fa6fc852608c727a9b90cb6135d6816bb907abe
Aloïs Deniel <alois.deniel@gmail.com>
2015-10-04 14:04:08 +0200

    Added commands.

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 var program = require('commander');
-var server = require('./server.js');
 
 /**
  * Client arguments.
  */
 
 program
-  .version('0.0.1')
+  .command('start')
+  .description('Starts the local server.')
   .option('-p, --port [type]', 'The server [port]', 5555)
+  .action(function(options){
+    var server = require('./server.js');
+    server.start(options.port);
+  });
+
+program
+  .command('import')
+  .description('Import a local backup into database.')
+  .option('-i, --import [file]', 'The backup [file]')
+  .action(function(options){
+    throw new Error('Not implemented');
+  });
+
+program
+  .command('export')
+  .description('Export database to a local file.')
+  .option('-e, --export [file]', 'The backup [file]')
+  .action(function(options){
+    throw new Error('Not implemented');
+  });
+
+program
+  .version('0.0.1')
   .parse(process.argv);
-
-// Starting server
-
-server.start(program.port);
