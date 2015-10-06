025147fd81e8d56794c46bfd1355a1f3e613d4f2
Aloïs Deniel <alois.deniel@gmail.com>
2015-10-06 14:49:33 +0200

    Added vue.js framework.

--- public/main.js
+++ public/main.js
@@ -1,89 +1,54 @@
-(function($) {
-    $.QueryString = (function(a) {
-        if (a == "") return {};
-        var b = {};
-        for (var i = 0; i < a.length; ++i)
-        {
-            var p=a[i].split('=');
-            if (p.length != 2) continue;
-            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
-        }
-        return b;
-    })(window.location.search.substr(1).split('&'))
-})(jQuery);
+var app = new Vue({
+  el: '#app',
+  data: {
+    cards: [],
+    tags: []
+  },
 
-// 1. Parsing snippets
+  watch: {
+    cards: function(val,oldVal) {
+      console.log('updated list')
+      hljs.initHighlightingOnLoad();
+      minigrid('#content', '.card', 10);
+    }
+  },
 
-var tags = $.QueryString["tags"];
+  methods: {
+    /*
+     * Loads distant cards and tags from api.
+     */
+    update: function() {
+      var vm = this;
+      $.get('/api/cards', function(data){
+        vm.cards = data.cards;
+      });
+    },
 
-if(tags)
-{
-  tags = tags.split(',');
-}
-else { tags = null; }
+    /*
+     * Adds a new card to the list.
+     */
+    addCard: function(card) {
 
-var allSnippets = [];
-var allTags = [];
+    },
 
-var snippetsNodes = $("script.snippet").each(function(index ) {
+    /*
+     * Edits a card.
+     */
+    editCard: function(card) {
 
-var snippetTags = $(this).data("tags").toString().split(',');
+    },
 
-var isValid = tags == null;
+    /*
+     * Removes an existing card from the list.
+     */
+     deleteCard: function(card) {
 
-if(!isValid)
-{
-  for (var i = 0; i < snippetTags.length && !isValid; i++) {
-    isValid = $.inArray(snippetTags[i],tags) >= 0;
+     }
   }
-}
-
-if(isValid)
-{
-  allSnippets.push({
-      title: $(this).data("title"),
-      lang: $(this).data("lang"),
-      description: $(this).data("description"),
-      tags: snippetTags,
-      content: $(this).html()
-  });
-}
-
 });
 
-// 2. Rendering view
+app.update();
 
-$("#tags li").each(function(index ) {
-  allTags.push($(this).html());
-  $(this).addClass("tag-"+(index+1))
-});
-
-var content = document.getElementById('content');
-
-function appendSnippet(content,lastSnippet){
-  lastSnippet.content = $('<div/>').text(lastSnippet.content).html();
-  var div = "<div class='card'><h2>";
-  var tagValues = "";
-  for(var i = 0;i < lastSnippet.tags.length;i++){
-
-    var index = parseInt(lastSnippet.tags[i]);
-
-    //div += "<div class='badge badge-" + index + "'></div>";
-    //tagValues += "<span class=tag-" + index + ">"+  allTags[index - 1] + "</span>";
-    div += "<span class='tag tag-" + ((index - 1)  % 10) + "''>"+  allTags[index - 1] + "</span>";
-  }
-
-  div += "<span>"+lastSnippet.title+"</span></h2><p>"+lastSnippet.description+"</p><pre><code class='"+lastSnippet.lang+"'>"+lastSnippet.content+"</code></pre></div>";
-  content.innerHTML += div;
-}
-
-for(var i = 0;i < allSnippets.length;i++){
-    appendSnippet(content,allSnippets[i]);
-}
-
-hljs.initHighlightingOnLoad();
-
-minigrid('#content', '.card', 10);
 window.addEventListener('resize', function(){
   minigrid('#content', '.card', 10);
 });
